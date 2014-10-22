fbfe4807a78c760109c8d5d24968b4162d12bd04
Graham Cooper <grahamcooper@live.com>
2014-10-21 22:22:44 -0700

    Google maps
    Async loading of google maps api and utilizing html5 location

--- client/src/components/home-page/home.js
+++ client/src/components/home-page/home.js
@@ -1,12 +1,33 @@
 define(["knockout", "text!./home.html", 'async!http://maps.google.com/maps/api/js?sensor=false'], function (ko, homeTemplate) {
 
 	function HomeViewModel(route) {
-		this.message = ko.observable('Welcome to Shoutmap!');
+		this.initialize();
 	}
 
-	HomeViewModel.prototype.doSomething = function () {
-		this.message('You invoked doSomething() on the viewmodel.');
-	};
+	HomeViewModel.prototype.initialize = function () {
+		var mapOptions = {
+			center: {
+				lat: -34.397,
+				lng: 150.644
+			},
+			zoom: 12
+		};
+		var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(function (position) {
+				var pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+				var infowindow = new google.maps.InfoWindow({
+					map: map,
+					position: pos,
+					content: 'Location found using HTML5.'
+				});
+
+				map.setCenter(pos);
+			});
+		}
+	}
 
 	return {
 		viewModel: HomeViewModel,
