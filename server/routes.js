664b5df2dd485595ea23a0dd381fc73ed1734e30
Graham Cooper <gcooper@tibco.com>
2014-10-29 13:13:07 -0700

    Separated models and cleaned up server
    Updated dist for client
    Introduced better logging
    updated routing client and server side

---
+++ server/routes.js
@@ -0,0 +1,80 @@
+var Shout = require('./models/shout'),
+    Reply = require('./models/reply');
+
+/* Initialize Routing */
+this.initialize = function (app) {
+    /* Get
+     * '/all'
+     * Returns all shouts from the database
+     * */
+    app.get("/api/shouts", function (req, res) {
+        Shout.find(function (err, shouts) {
+            res.send(shouts);
+        });
+    });
+
+    /* Get
+     * '/replies'
+     * Returns all replies from the database
+     * */
+    app.get("/api/replies", function (req, res) {
+        Reply.find(function (err, replies) {
+            res.send(replies);
+        });
+    });
+
+    /* Get
+     * '/replies/{id}'
+     * Returns all replies for a shout from the database
+     * */
+    app.get("/api/replies/:id", function (req, res) {
+        Reply.find({parentId: req.params.id}, function (err, replies) {
+            res.send(replies);
+        });
+    });
+
+    /* Post
+     * '/shout'
+     * Saves a new Shout object to the database*/
+    app.post('/api/shout', function (req, res) {
+        var obj = req.body;
+        var shout = new Shout({
+            text: obj.text,
+            location: {
+                lat: obj.location.lat,
+                long: obj.location.long
+            },
+            time: obj.time
+        });
+        shout.save(function (err) {
+            if (err) {
+                console.log('Error saving shout to database - ' + err);
+            }
+            res.send();
+        })
+    });
+
+    /* Post
+     * '/reply'
+     * Saves a new Reply object to the database*/
+    app.post('/api/reply', function (req, res) {
+        var obj = req.body;
+        var reply = new Reply({
+            text: obj.text,
+            location: {
+                lat: obj.location.lat,
+                long: obj.location.long
+            },
+            time: obj.time,
+            parentId: obj.parentId
+        });
+        reply.save(function (err) {
+            if (err) {
+                console.log('Error saving reply to database - ' + err);
+            }
+            res.send();
+        })
+    });
+};
+
+exports.initialize = this.initialize;
--- server/server.js
+++ server/server.js
@@ -1,112 +1,37 @@
 // Dependencies
-var express = require('express');
-var mongoose = require('mongoose');
-var cors = require('cors');
-var bodyParser = require('body-parser');
+var http = require('http'),
+    express = require('express'),
+    mongoose = require('mongoose'),
+    cors = require('cors'),
+    bodyParser = require('body-parser'),
+    logger = require('./logger'),
+    path = require('path'),
+    routes = require('./routes.js');
 
 // Dependency instantiation
 console.log('App starting up...');
 var app = express();
+
+app.set('port', process.env.PORT || 3000);
+
 app.use(cors());
 app.use(bodyParser());
+app.use(logger);
 
-console.log('Connecting to database');
+app.use(express.static(path.join(__dirname, '../client/dist/')));
+
+routes.initialize(app);
+
+// Connect to the backing database
+console.log('Connecting to database...');
 mongoose.connect('mongodb://localhost/shoutmap');
-
-// Model definition
-var Shout = mongoose.model('Shout', {
-    text: String,
-    location: {
-        lat: Number,
-        long: Number
-    },
-    time: Date
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Connection error:'));
+db.once('open', function callback() {
+    console.log('Connected to MongoDB: ' + db.name);
 });
 
-var Reply = mongoose.model('Reply', {
-    text: String,
-    location: {
-        lat: Number,
-        long: Number
-    },
-    time: Date,
-    parentId: String
+// Create the server from the app and listen to the given port
+http.createServer(app).listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
 });
-/* Routing */
-
-/* Get
- * '/all'
- * Returns all shouts from the database
- * */
-app.get("/shouts", function (req, res) {
-    Shout.find(function (err, shouts) {
-        res.send(shouts);
-    });
-});
-
-/* Get
- * '/replies'
- * Returns all replies from the database
- * */
-app.get("/replies", function (req, res) {
-    Reply.find(function (err, replies) {
-        res.send(replies);
-    });
-});
-
-/* Get
- * '/replies/{id}'
- * Returns all replies for a shout from the database
- * */
-app.get("/replies/:id", function (req, res) {
-    Reply.find({parentId: req.params.id},function (err, replies) {
-        res.send(replies);
-    });
-});
-
-/* Post
- * '/shout'
- * Saves a new Shout object to the database*/
-app.post('/shout', function (req, res) {
-    var obj = req.body;
-    var shout = new Shout({
-        text: obj.text,
-        location: {
-            lat: obj.location.lat,
-            long: obj.location.long
-        },
-        time: obj.time
-    });
-    shout.save(function (err) {
-        if (err) {
-            console.log('Error saving shout to database - ' + err);
-        }
-        res.send();
-    })
-});
-
-/* Post
- * '/reply'
- * Saves a new Reply object to the database*/
-app.post('/reply', function (req, res) {
-    var obj = req.body;
-    var reply = new Reply({
-        text: obj.text,
-        location: {
-            lat: obj.location.lat,
-            long: obj.location.long
-        },
-        time: obj.time,
-        parentId: obj.parentId
-    });
-    reply.save(function (err) {
-        if (err) {
-            console.log('Error saving reply to database - ' + err);
-        }
-        res.send();
-    })
-});
-
-// Listen for any requests on port 3000
-console.log('Listening on port 3000');
-app.listen(3000);
