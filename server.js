82fbedf08d129dff844a74c3ce221414f7c98fc8
Graham Cooper <gcooper@tibco.com>
2014-10-29 18:44:11 -0700

    updated runtime config in server.js

--- server.js
+++ server.js
@@ -24,7 +24,8 @@ routes.initialize(app);
 
 // Connect to the backing database
 logger.log('Connecting to database...');
-mongoose.connect('mongodb://heroku:qYibqHAQWLaV34s0sW7MO670mFF9Ap_0MTXE6E9N4vqvIEzhhvy7aeXaToyMCBH7lBjizjDXYrkZEmMpSY2wmw@dogen.mongohq.com:10022/app31106665');
+var dbUrl = process.env.NODE_ENV === "production" ? process.env.MONGOHQ_URL : 'mongodb://localhost/shoutmap';
+mongoose.connect(dbUrl);
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Connection error:'));
 db.once('open', function callback() {
